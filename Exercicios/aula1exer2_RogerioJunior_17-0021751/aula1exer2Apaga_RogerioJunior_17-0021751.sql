bd4e11ef0b0a7526369449925d7767b03121a5d1
Rogério Júnior <junior.rogerio8@gmail.com>
2020-03-11 01:36:12 -0300

    V1 of physical and the others scripts with some necessary adjustments on the diagrams

---
+++ Exercicios/aula1exer2_RogerioJunior_17-0021751/aula1exer2Apaga_RogerioJunior_17-0021751.sql
@@ -0,0 +1,27 @@
+-- ---------------------   << Empresa de Vendas  >>   ---------------------
+--
+--                    SCRIPT DE APAGAR (DDL)
+-- 
+-- Data Criacao ...........: 11/03/2020
+-- Autor(es) ..............: Rogério S. dos Santos Júnior
+-- Banco de Dados .........: MySQL 8.0
+-- Banco de Dados(nome) ...: aula1exer2
+-- 
+-- 
+-- PROJETO => 01 Base de Dados
+--         => 10 Tabelas
+-- 
+-- -------------------------------------------------------------------------
+
+USE aula1exer2;
+
+DROP TABLE contem;
+DROP TABLE telefone;
+DROP TABLE inserido;
+DROP TABLE supervisiona;
+DROP TABLE VENDA;
+DROP TABLE AREA;
+DROP TABLE PRODUTO;
+DROP TABLE EMPREGADO;
+DROP TABLE GERENTE;
+DROP TABLE PESSOA;
--- Exercicios/aula1exer2_RogerioJunior_17-0021751/aula1exer2Fisico_RogerioJunior_17-0021751.sql
+++ Exercicios/aula1exer2_RogerioJunior_17-0021751/aula1exer2Fisico_RogerioJunior_17-0021751.sql
@@ -1,114 +1,127 @@
-/* aula1exer2Logico_RogerioJunior_17-0021751: */
+-- ---------------------   << Empresa de Vendas  >>   ---------------------
+--
+--                    SCRIPT DE CRIAÇÃO (DDL)
+-- 
+-- Data Criacao ...........: 11/03/2020
+-- Autor(es) ..............: Rogério S. dos Santos Júnior
+-- Banco de Dados .........: MySQL 8.0
+-- Banco de Dados(nome) ...: aula1exer2
+-- 
+-- 
+-- PROJETO => 01 Base de Dados
+--         => 10 Tabelas
+-- 
+-- -------------------------------------------------------------------------
+
+CREATE DATABASE IF NOT EXISTS aula1exer2
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+USE aula1exer2;
 
 CREATE TABLE PESSOA (
-    nome VARCHAR(100),
-    senha VARCHAR(50),
-    cpf BIGINT(11) PRIMARY KEY
-);
+    cpf BIGINT(11) NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    senha VARCHAR(50) NOT NULL,
+
+    CONSTRAINT PESSOA_PK PRIMARY KEY (cpf)
+) ENGINE = InnoDB;
 
 CREATE TABLE GERENTE (
-    idGerente INT(4) PRIMARY KEY,
-    formacao VARCHAR(15),
-    email VARCHAR(30),
-    cpf BIGINT(11)
-);
+    idGerente INT(4) AUTO_INCREMENT NOT NULL,
+    formacao VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    cpf BIGINT(11) NOT NULL,
 
-CREATE TABLE VENDA (
-    idVenda INT(4) PRIMARY KEY,
-    data DATETIME,
-    matriculaEmpregado INT(4)
-);
+    CONSTRAINT GERENTE_PK PRIMARY KEY (idGerente),
 
-CREATE TABLE AREA (
-    idArea INT(4) PRIMARY KEY,
-    nome VARCHAR(30),
-    idGerente INT(4)
-);
+    CONSTRAINT GERENTE_PESSOA_FK FOREIGN KEY (cpf)
+        REFERENCES PESSOA (cpf)
+) ENGINE = InnoDB  AUTO_INCREMENT = 1;
 
 CREATE TABLE EMPREGADO (
-    matriculaEmpregado INT(4) PRIMARY KEY,
-    rua VARCHAR(20),
-    numero INT(5),
-    bairro VARCHAR(20),
+    matriculaEmpregado INT(4) AUTO_INCREMENT NOT NULL,
+    rua VARCHAR(20) NOT NULL,
+    numero INT(5) NOT NULL,
+    bairro VARCHAR(20) NOT NULL,
     complemento VARCHAR(50),
-    cidade VARCHAR(30),
-    estado CHAR(2),
-    cep INT(7),
-    cpf BIGINT(11)
-);
+    cidade VARCHAR(30) NOT NULL,
+    estado CHAR(2) NOT NULL,
+    cep INT(7) NOT NULL,
+    cpf BIGINT(11) NOT NULL,
+
+    CONSTRAINT EMPREGADO_PK PRIMARY KEY (matriculaEmpregado),
+
+    CONSTRAINT EMPREGADO_PESSOA_FK FOREIGN KEY (cpf)
+        REFERENCES PESSOA (cpf)
+) ENGINE = InnoDB  AUTO_INCREMENT = 1;
+
+CREATE TABLE VENDA (
+    idVenda INT(4) AUTO_INCREMENT NOT NULL,
+    data DATETIME NOT NULL,
+    matriculaEmpregado INT(4) NOT NULL,
+
+    CONSTRAINT VENDA_PK PRIMARY KEY (idVenda),
+    
+    CONSTRAINT VENDA_EMPREGADO_FK FOREIGN KEY (matriculaEmpregado)
+        REFERENCES EMPREGADO(matriculaEmpregado)
+) ENGINE = InnoDB  AUTO_INCREMENT = 1;
+
+CREATE TABLE AREA (
+    idArea INT(4) AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(30) NOT NULL,
+    idGerente INT(4) NOT NULL,
+
+    CONSTRAINT AREA_PK PRIMARY KEY (idArea),
+
+    CONSTRAINT AREA_GERENTE_FK FOREIGN KEY (idGerente)
+        REFERENCES GERENTE(idGerente)
+) ENGINE = InnoDB  AUTO_INCREMENT = 1;
 
 CREATE TABLE PRODUTO (
-    idProduto INT(4) PRIMARY KEY,
-    nome VARCHAR(30),
-    precoUnitario FLOAT(7,2)
-);
+    idProduto INT(4) AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(30) NOT NULL,
+    precoUnitario FLOAT(7,2) NOT NULL,
+
+    CONSTRAINT PRODUTO_PK PRIMARY KEY (idProduto)
+) ENGINE = InnoDB  AUTO_INCREMENT = 1;
 
 CREATE TABLE telefone (
     matriculaEmpregado INT(4) NOT NULL,
-    telefone BIGINT(12),
-    UNIQUE (matriculaEmpregado, telefone)
-);
+    telefone BIGINT(12) NOT NULL,
+
+    CONSTRAINT telefone_UK UNIQUE (matriculaEmpregado, telefone),
+
+    CONSTRAINT telefone_EMPREGADO_FK FOREIGN KEY (matriculaEmpregado)
+        REFERENCES EMPREGADO(matriculaEmpregado)
+) ENGINE = InnoDB;
 
 CREATE TABLE inserido (
-    matriculaEmpregado INT(4),
-    idArea INT(4)
-);
+    matriculaEmpregado INT(4) NOT NULL,
+    idArea INT(4) NOT NULL,
+
+    CONSTRAINT inserido_EMPREGADO_FK FOREIGN KEY (matriculaEmpregado)
+        REFERENCES EMPREGADO(matriculaEmpregado),
+    CONSTRAINT inserido_AREA_FK FOREIGN KEY (idArea)
+        REFERENCES AREA(idArea)
+) ENGINE = InnoDB;
 
 CREATE TABLE supervisiona (
-    idGerente INT(4),
-    matriculaEmpregado INT(4)
-);
+    idGerente INT(4) NOT NULL,
+    matriculaEmpregado INT(4) NOT NULL,
+
+    CONSTRAINT supervisiona_GERENTE_FK FOREIGN KEY (idGerente)
+        REFERENCES GERENTE(idGerente),
+    CONSTRAINT supervisiona_EMPREGADO_FK FOREIGN KEY (matriculaEmpregado)
+        REFERENCES EMPREGADO(matriculaEmpregado)
+) ENGINE = InnoDB;
 
 CREATE TABLE contem (
-    idVenda INT(4),
-    idProduto INT(4)
-);
- 
-ALTER TABLE GERENTE ADD CONSTRAINT FK_GERENTE_2
-    FOREIGN KEY (cpf)
-    REFERENCES PESSOA (cpf)
-    ON DELETE CASCADE;
- 
-ALTER TABLE VENDA ADD CONSTRAINT FK_VENDA_2
-    FOREIGN KEY (matriculaEmpregado)
-    REFERENCES EMPREGADO (matriculaEmpregado);
- 
-ALTER TABLE AREA ADD CONSTRAINT FK_AREA_2
-    FOREIGN KEY (idGerente)
-    REFERENCES GERENTE (idGerente);
- 
-ALTER TABLE EMPREGADO ADD CONSTRAINT FK_EMPREGADO_2
-    FOREIGN KEY (cpf)
-    REFERENCES PESSOA (cpf)
-    ON DELETE CASCADE;
- 
-ALTER TABLE telefone ADD CONSTRAINT FK_telefone_1
-    FOREIGN KEY (matriculaEmpregado)
-    REFERENCES EMPREGADO (matriculaEmpregado);
- 
-ALTER TABLE inserido ADD CONSTRAINT FK_inserido_1
-    FOREIGN KEY (idArea)
-    REFERENCES AREA (idArea)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE inserido ADD CONSTRAINT FK_inserido_2
-    FOREIGN KEY (matriculaEmpregado)
-    REFERENCES EMPREGADO (matriculaEmpregado);
- 
-ALTER TABLE supervisiona ADD CONSTRAINT FK_supervisiona_1
-    FOREIGN KEY (idGerente)
-    REFERENCES GERENTE (idGerente);
- 
-ALTER TABLE supervisiona ADD CONSTRAINT FK_supervisiona_2
-    FOREIGN KEY (matriculaEmpregado)
-    REFERENCES EMPREGADO (matriculaEmpregado);
- 
-ALTER TABLE contem ADD CONSTRAINT FK_contem_1
-    FOREIGN KEY (idVenda)
-    REFERENCES VENDA (idVenda)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE contem ADD CONSTRAINT FK_contem_2
-    FOREIGN KEY (idProduto)
-    REFERENCES PRODUTO (idProduto)
-    ON DELETE RESTRICT;
+    idVenda INT(4) NOT NULL,
+    idProduto INT(4) NOT NULL,
+    quantidade INT(3) NOT NULL,
+
+    CONSTRAINT contem_VENDA_FK FOREIGN KEY (idVenda)
+        REFERENCES VENDA (idVenda),
+    CONSTRAINT contem_PRODUTO_FK FOREIGN KEY (idProduto)
+        REFERENCES PRODUTO (idProduto)
+) ENGINE = InnoDB;
